d8929c05bb3fdc100becd794e221b477372885b1
eddeT <teodornoren@gmail.com>
2021-12-02 10:37:00 +0100

    finalized

--- d2/dive.py
+++ d2/dive.py
@@ -1,32 +1,49 @@
 
 input0 = []
-with open("demo.dat") as f:
+with open("dive.dat") as f:
     for line in f:
         line = line.rstrip('\n')
         input0.append(line)
 
-print(input0)
-print(input0[0].split())
-
-class Submarine:
+class SubmarineAlfa:
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        self.horizontal = 0
+        self.depth = 0
 
     def dive(self, dir, amount):
         if "forward" in dir:
-            self.x += amount
+            self.horizontal += amount
         elif "down" in dir:
-            self.y += amount
+            self.depth += amount
         elif "up" in dir:
-            self.y -= amount
+            self.depth -= amount
 
 
     def __str__(self):
-        return f"We are at x = {self.x} & y = {self.y}\n\
-            Product = {self.x * self.y}"
+        return f"Horizontal = {self.horizontal} Depth = {self.depth}\n\
+            Product = {self.horizontal * self.depth}"
+
+class SubmarineBeta:
+    def __init__(self):
+        self.horizontal = 0
+        self.depth = 0
+        self.aim = 0
+
+    def dive(self, dir, amount):
+        if "forward" in dir:
+            self.horizontal += amount
+            self.depth += self.aim * amount
+        elif "down" in dir:
+            self.aim += amount
+        elif "up" in dir:
+            self.aim -= amount
+
+
+    def __str__(self):
+        return f"Horizontal = {self.horizontal} Depth = {self.depth} Aim = {self.aim}\n\
+            Product = {self.horizontal * self.depth}"
         
-yellowSubmarine = Submarine()
+yellowSubmarine = SubmarineBeta()
 for data in input0:
     d2 = data.split()
     yellowSubmarine.dive(d2[0], int(d2[1]))
